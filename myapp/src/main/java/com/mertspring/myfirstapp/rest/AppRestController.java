6765157161c4f0a277f79926372af9827ae1dc20
mertcekuc <cekucmert@gmail.com>
2025-06-21 15:41:06 +0300

    Disabled security, set config and preporties

--- myapp/src/main/java/com/mertspring/myfirstapp/rest/AppRestController.java
+++ myapp/src/main/java/com/mertspring/myfirstapp/rest/AppRestController.java
@@ -1,9 +1,14 @@
 package com.mertspring.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppRestController {
+    @Value("${benim.adim}")
+    String ad;
+    @Value("${benim.soyadim}")
+    String soyad;
 
     @GetMapping("/")
     public String SayHello(){
@@ -18,5 +23,10 @@ public class AppRestController {
     @GetMapping("/new2")
     public String saySecond(){
         return "This is second after devtool";
+    }
+
+    @GetMapping("/selam")
+    public String saySelam(){
+        return "selam benim adim " + ad + " " +soyad;
     }
 }
