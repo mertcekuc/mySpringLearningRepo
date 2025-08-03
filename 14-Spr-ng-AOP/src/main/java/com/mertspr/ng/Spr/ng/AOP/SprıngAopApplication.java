fccbcf5544927b385dcb23f8577261a42ee364ba
mertcekuc <cekucmert@gmail.com>
2025-08-03 16:51:47 +0300

    aop project setup

---
+++ 14-Spr-ng-AOP/src/main/java/com/mertspr/ng/Spr/ng/AOP/SprıngAopApplication.java
@@ -0,0 +1,22 @@
+package com.mertspr.ng.Spr.ng.AOP;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SprıngAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprıngAopApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+			System.out.println("Hello world");
+		};
+
+	}
+}
---
+++ 14-Spr-ng-AOP/src/test/java/com/mertspr/ng/Spr/ng/AOP/SprıngAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mertspr.ng.Spr.ng.AOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprıngAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
