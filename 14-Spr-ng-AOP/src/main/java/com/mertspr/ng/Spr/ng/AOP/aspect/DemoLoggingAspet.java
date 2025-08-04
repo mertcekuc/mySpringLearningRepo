f78a9297de3adf3ffc49b233e3e8526233c0c966
mertcekuc <cekucmert@gmail.com>
2025-08-04 19:44:01 +0300

    pointcut devlaration

--- 14-Spr-ng-AOP/src/main/java/com/mertspr/ng/Spr/ng/AOP/aspect/DemoLoggingAspet.java
+++ 14-Spr-ng-AOP/src/main/java/com/mertspr/ng/Spr/ng/AOP/aspect/DemoLoggingAspet.java
@@ -1,7 +1,9 @@
 package com.mertspr.ng.Spr.ng.AOP.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,8 +15,19 @@ public class DemoLoggingAspet {
     //@Before("execution(* add*())") // any return type
     //@Before("execution(* add*(..))")// zero or more params
     //@Before("execution(* add*(*,*))") //exact 2 prams
-    @Before("execution(* com.mertspr.ng.Spr.ng.AOP.dao.*.*(..))") //all methods inb a package
+    @Before("customPointCut()") //all methods inb a package
     public void beforeAddAccountAdvice(){
+        System.out.println(getClass() + " Before executing adding account advice!!!!!");
+    }
+
+    @After("customPointCut()")
+    public void afterAddAccountAdvice(){
+        System.out.println(getClass() + " After executing adding account advice!!!!!");
+
+    }
+
+    @Pointcut("execution(* add*(..))")
+    public void customPointCut() {
         System.out.println(getClass() + "Before executing adding account advice!!!!!");
     }
 }
